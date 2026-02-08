ac1df48089646cd90a90b6e0b894b111a550bd91
abdulghanivst <abdulghanivst@gmail.com>
2026-02-08 22:40:47 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-column = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
-df = pd.read_csv(url, names=column_names)
-print("Dataset Shape:", df.shape)
-print(df.head())
